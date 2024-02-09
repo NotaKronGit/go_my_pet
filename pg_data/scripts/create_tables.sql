c1d908de3d3685df70af12867448413e1ec724cb
Samofalov Sergey <samofalov.sv@exmo.com>
2024-02-09 12:01:58 +0300

    git rm -r --cached .

--- pg_data/scripts/create_tables.sql
+++
@@ -1,8 +0,0 @@
--- create users table
-CREATE TABLE IF NOT EXISTS USERS (
-    id INT PRIMARY KEY,
-    name VARCHAR(10)
-);
-
--- insert initial default users
-INSERT INTO USERS (id,name) VALUES (0,'USER'),(1,'ADMIN');
